106636e7865fd90d0c7b9a3b8e8560c72dc1cf0f
Bernardo <bma.reis@campus.fct.unl.pt>
2021-12-30 00:20:39 +0000

    QuerryAction2.java Finished, StandardActions.java modified, JabRefFrame.java modified

---
+++ src/main/java/org/jabref/gui/help/QuerryAction2.java
@@ -0,0 +1,67 @@
+package org.jabref.gui.help;
+
+
+import javafx.util.Pair;
+import org.jabref.gui.DialogService;
+import org.jabref.gui.actions.SimpleCommand;
+
+import com.airhacks.afterburner.injection.Injector;
+import org.jabref.gui.search.RebuildFulltextSearchIndexAction;
+import org.jabref.model.database.BibDatabase;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+import java.util.Map;
+
+public class QuerryAction2 extends SimpleCommand {
+
+    RebuildFulltextSearchIndexAction.GetCurrentLibraryTab currentLibraryTab;
+
+    public QuerryAction2(RebuildFulltextSearchIndexAction.GetCurrentLibraryTab aux) {
+        currentLibraryTab = aux;
+    }
+
+    @Override
+    public void execute() {
+        final JFrame frame = new JFrame();
+
+        frame.setBounds(200,200, 400,300);
+        Container container = frame.getContentPane();
+        container.setLayout(null);
+
+
+        JLabel logo  = new JLabel("Query - Common Articles");
+        logo.setBounds(10,5,400,50);
+
+        JLabel author  = new JLabel("Author");
+        author.setBounds(10,35,100,50);
+
+        JTextField text = new JTextField("");
+        text.setBounds(10,70,300,35);
+
+        JButton button = new JButton("Confirmar");
+        button.setBounds(10,150,100, 40);
+
+        container.add(logo);
+        container.add(author);
+        container.add(text);
+        container.add(button);
+
+        button.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                String name = text.getText();
+
+                Map<String, List<String>> list = currentLibraryTab.get().getBibDatabaseContext().getDatabase().getCommonArticles(name);
+                String result = list.toString();
+                JOptionPane.showMessageDialog(frame, result);
+            }
+        });
+
+        frame.setVisible(true);
+
+    }
+
+
+}

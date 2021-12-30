36e7c5dd1b61cd1ef6e333a99d8694709bdc90a8
Martim <ms.gouveia@campus.fct.unl.pt>
2021-12-30 01:37:20 +0000

    Article information tab

--- src/main/java/org/jabref/gui/entryeditor/EntryEditor.java
+++ src/main/java/org/jabref/gui/entryeditor/EntryEditor.java
@@ -244,6 +244,9 @@ public class EntryEditor extends BorderPane {
         //Journal information
         entryEditorTabs.add(new JournalInformationTab(databaseContext, libraryTab.getSuggestionProviders(), undoManager, dialogService, preferencesService, stateManager, Globals.entryTypesManager, ExternalFileTypes.getInstance(), Globals.TASK_EXECUTOR, Globals.journalAbbreviationRepository));
 
+        //Article information tab
+        entryEditorTabs.add(new ArticleInformationTab(databaseContext, libraryTab.getSuggestionProviders(), undoManager, dialogService, preferencesService, stateManager, Globals.entryTypesManager, ExternalFileTypes.getInstance(), Globals.TASK_EXECUTOR, Globals.journalAbbreviationRepository));
+
         // Optional fields
         entryEditorTabs.add(new OptionalFieldsTab(databaseContext, libraryTab.getSuggestionProviders(), undoManager, dialogService, preferencesService, stateManager, Globals.entryTypesManager, ExternalFileTypes.getInstance(), Globals.TASK_EXECUTOR, Globals.journalAbbreviationRepository));
         entryEditorTabs.add(new OptionalFields2Tab(databaseContext, libraryTab.getSuggestionProviders(), undoManager, dialogService, preferencesService, stateManager, Globals.entryTypesManager, ExternalFileTypes.getInstance(), Globals.TASK_EXECUTOR, Globals.journalAbbreviationRepository));
